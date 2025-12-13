feeaf3155d7fcbba90537814fecc3eaff0066068
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-12-13 14:58:06 +0100

    Refactor input handling for roads and interval; add input validation

--- Traffic Light Simulator with Java/task/src/traffic/Main.java
+++ Traffic Light Simulator with Java/task/src/traffic/Main.java
@@ -7,14 +7,58 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the traffic management system!");
-        System.out.print("Input the number of roads: ");
-        int numberOfRoads = sc.nextInt();
-        System.out.print("Input the interval: ");
-        int interval = sc.nextInt();
+        getNumberOfRoads(sc);
+        getInterval(sc);
+        clearConsole();
 
         boolean running = true;
         while (running) {
             running = handleMenu();
+        }
+    }
+
+    /**
+     * Interval must be > 0
+     *
+     * @param sc
+     */
+    private static void getInterval(Scanner sc) {
+        System.out.print("Input the interval: ");
+        while (true) {
+
+            try {
+                int interval = sc.nextInt();
+                if (interval > 0) {
+                    break;
+                } else {
+                    System.out.print("Incorrect input. Please try again:");
+                }
+            } catch (Exception e) {
+                System.out.print("Incorrect input. Please try again:");
+                sc.next(); // clear invalid input
+            }
+        }
+    }
+
+    /**
+     * Number of roads must be > 0
+     *
+     * @param sc
+     */
+    private static void getNumberOfRoads(Scanner sc) {
+        System.out.print("Input the number of roads: ");
+        try {
+            while (true) {
+                int numberOfRoads = sc.nextInt();
+                if (numberOfRoads > 0) {
+                    break;
+                } else {
+                    System.out.print("Incorrect input. Please try again:");
+                }
+            }
+        } catch (Exception e) {
+            System.out.print("Incorrect input. Please try again:");
+            sc.next(); // clear invalid input
         }
     }
 
@@ -41,10 +85,15 @@ public class Main {
                 System.out.println("Bye!");
                 return false;
             default:
-                System.out.println("Invalid option. Please try again.");
+                System.out.println("Incorrect option. Please try again.");
         }
         sc.nextLine(); // Consume newline
         sc.nextLine();
+        clearConsole();
+        return true;
+    }
+
+    private static void clearConsole() {
         try {
             var clearCommand = System.getProperty("os.name").contains("Windows")
                     ? new ProcessBuilder("cmd", "/c", "cls")
@@ -52,7 +101,6 @@ public class Main {
             clearCommand.inheritIO().start().waitFor();
         } catch (IOException | InterruptedException ignored) {
         }
-        return true;
     }
 
 }
